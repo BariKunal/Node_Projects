81d5cea432def4c96da6daf6c7bd10f1d48c436f
BariKunal <kunalbari751@gmail.com>
2025-06-09 11:45:24 +0530

    logics for calculate bill using quantity and rate

---
+++ Bill_Calculation/index.js
@@ -0,0 +1,21 @@
+let express = require("express")
+let bodyParser = require("body-parser")
+let app = express()
+app.set("view engine","ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",(req,res) => {
+    res.render("bill.ejs",{calBill:0})
+})
+
+app.post("/save",(req,res) => {
+    let {qty,rate} = req.body
+    let b = qty*rate;
+    res.render("bill.ejs",{calBill:b})
+})
+
+app.listen(3000, () =>{
+    console.log("Server Started..");
+    
+})
