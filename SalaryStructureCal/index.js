67706d1beecca27633ccdbcccf5a6873ca6860a0
BariKunal <kunalbari751@gmail.com>
2025-06-10 14:15:56 +0530

    index.js file for routings

---
+++ SalaryStructureCal/index.js
@@ -0,0 +1,23 @@
+let express = require("express")
+let bodyParser = require("body-parser")
+let app = express()
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.render("salary.ejs", { name:null, email:null, contact:0, basicSal:0, da:0, hra:0,totalSal:0 })
+})
+
+app.post("/save", (req, res) => {
+    let { name,email,contact,basicSal } = req.body
+    let da = basicSal * 0.30
+    let hra = basicSal * 0.30
+    let totalSal = da + hra + basicSal
+    res.render("salary.ejs", {name,email,contact,basicSal,da,hra,totalSal})
+})
+
+app.listen(3000, () => {
+    console.log("Server Started..");
+
+})
