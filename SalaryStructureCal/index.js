c3b58df8340e7b11ac40128b2f4962fbf5480ca0
BariKunal <kunalbari751@gmail.com>
2025-06-10 14:17:35 +0530

    Js router for body parser

--- SalaryStructureCal/index.js
+++ SalaryStructureCal/index.js
@@ -6,15 +6,15 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
-    res.render("salary.ejs", { name:null, email:null, contact:0, basicSal:0, da:0, hra:0,totalSal:0 })
+    res.render("salary.ejs", { name: null, email: null, contact: 0, basicSal: 0, da: 0, hra: 0, totalSal: 0 })
 })
 
 app.post("/save", (req, res) => {
-    let { name,email,contact,basicSal } = req.body
+    let { name, email, contact, basicSal } = req.body
     let da = basicSal * 0.30
     let hra = basicSal * 0.30
     let totalSal = da + hra + basicSal
-    res.render("salary.ejs", {name,email,contact,basicSal,da,hra,totalSal})
+    res.render("salary.ejs", { name, email, contact, basicSal, da, hra, totalSal })
 })
 
 app.listen(3000, () => {
