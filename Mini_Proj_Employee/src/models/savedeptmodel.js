dcee5647decfdfae457f5e97fb987bf15e3f4bad
BariKunal <kunalbari751@gmail.com>
2025-06-13 12:21:50 +0530

    Query Data for new dept add

--- Mini_Proj_Employee/src/models/savedeptmodel.js
+++ Mini_Proj_Employee/src/models/savedeptmodel.js
@@ -2,14 +2,14 @@ let db = require("../../db.js")
 exports.saveDept = (deptname) => {
     console.log(deptname);
     return new Promise((resolve,reject) => {
-        db.query("insert into dept values('0',?)",[deptname],(err,result) =>{
+        db.query("INSERT INTO dept VALUES ('0',?)", [deptname], (err, result) =>{
             if(err){
-                console.log(err);
-                reject("Dept not Save...")
+                console.log("Error IS : "+err);
+                reject("Department not Saved...")
             }
             else{
                 console.log(result);
-                resolve("Dept Save Successfully...")
+                resolve("Department Save Successfully...")
             }
         })
     })
