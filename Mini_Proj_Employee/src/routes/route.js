d30358d9b466a346dca52aa17dfd60aabd9c4cd1
BariKunal <kunalbari751@gmail.com>
2025-06-13 23:00:01 +0530

    Router for view data in department table

--- Mini_Proj_Employee/src/routes/route.js
+++ Mini_Proj_Employee/src/routes/route.js
@@ -3,7 +3,9 @@ let deptctrl = require("../controllers/deptcontroller.js")
 let router = express.Router()
 
 router.post("/adddept",deptctrl.saveDept)
+
 router.get("/",deptctrl.homePage)
 router.get("/newDept",deptctrl.newDept)
+router.get("/viewalldept",deptctrl.getAllDept)
 
 module.exports = router
