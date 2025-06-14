0a206fcd61cb516f95cbc97364ec1afbfdbfab17
BariKunal <kunalbari751@gmail.com>
2025-06-14 10:58:39 +0530

    Set controller for delete dept using id

--- Mini_Proj_Employee/src/controllers/deptcontroller.js
+++ Mini_Proj_Employee/src/controllers/deptcontroller.js
@@ -1,30 +1,48 @@
 let deptmodel = require("../models/savedeptmodel.js")
 
-exports.saveDept=((req,res) =>{
-    let {name} = req.body
+exports.saveDept = ((req, res) => {
+    let { name } = req.body
     let promise = deptmodel.saveDept(name)
     promise.then((result) => {
         console.log(result);
-        res.render("adddept.ejs",{msg:result})
+        res.render("adddept.ejs", { msg: result })
     }).catch((err) => {
-        res.render("adddept.ejs",{msg:err})
+        res.render("adddept.ejs", { msg: err })
     })
 })
 
-exports.homePage = (req,res) => {
+exports.homePage = (req, res) => {
     res.render("home.ejs")
 }
 
 exports.newDept = (req, res) => {
-    res.render("adddept.ejs",{msg:""})
+    res.render("adddept.ejs", { msg: "" })
 }
 
-exports.getAllDept = (req,res) => {
-    let promise   = deptmodel.getAllDept()
+exports.getAllDept = (req, res) => {
+    let promise = deptmodel.getAllDept()
     promise.then((result) => {
-        res.render("viewdept.ejs",{deptList:result})
+        res.render("viewdept.ejs", { deptList: result })
     })
     promise.catch((err) => {
         res.send(err)
     })
+}
+
+exports.delDept = (req, res) => {
+    let did = parseInt(req.query.did)
+    let promise = deptmodel.delDeptById(did)
+    promise.then((result) => {
+        let pro = deptmodel.getAllDept()
+        pro.then((result) => {
+            res.render("viewdept.ejs", { deptList: result })
+        })
+        pro.catch((err) => {
+            res.send(err)
+        })
+    })
+    promise.catch((err) => {
+        res.send(err)
+    })
+
 }
