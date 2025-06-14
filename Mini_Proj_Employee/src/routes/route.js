fcebaa1c03a72c921c5b6559eced7dc1fcfd3e0c
BariKunal <kunalbari751@gmail.com>
2025-06-14 10:59:55 +0530

    Router implementation for delete API

--- Mini_Proj_Employee/src/routes/route.js
+++ Mini_Proj_Employee/src/routes/route.js
@@ -7,5 +7,6 @@ router.post("/adddept",deptctrl.saveDept)
 router.get("/",deptctrl.homePage)
 router.get("/newDept",deptctrl.newDept)
 router.get("/viewalldept",deptctrl.getAllDept)
+router.get("/deptdelet",deptctrl.delDept)
 
 module.exports = router
