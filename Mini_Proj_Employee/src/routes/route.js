6c4fbff07e17cfeb4f9cdb788c23ff066a894def
BariKunal <kunalbari751@gmail.com>
2025-06-15 12:21:30 +0530

    Route implementation for search dept using ajax calling

--- Mini_Proj_Employee/src/routes/route.js
+++ Mini_Proj_Employee/src/routes/route.js
@@ -1,5 +1,6 @@
 let express = require("express")
 let deptctrl = require("../controllers/deptcontroller.js")
+let empCtrl = require("../controllers/empctrl.js")
 let router = express.Router()
 
 router.post("/adddept",deptctrl.saveDept)
@@ -10,4 +11,9 @@ router.get("/viewalldept",deptctrl.getAllDept)
 router.get("/deptdelet",deptctrl.delDept)
 router.get("/upddept",deptctrl.updDept)
 router.post("/updatedept",deptctrl.deptFinalUpd)
+router.get("/searchDeptByName",deptctrl.searchDeptByUsingName)
+
+router.get("/newemployee",empCtrl.newEmp)
+
+
 module.exports = router
