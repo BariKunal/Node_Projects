e39dc65a6d51c0f427cac3880ca93f9d08f0ca2b
BariKunal <kunalbari751@gmail.com>
2025-06-16 12:33:28 +0530

    implements the controller for emloyee add and view

--- Mini_Proj_Employee/src/controllers/empctrl.js
+++ Mini_Proj_Employee/src/controllers/empctrl.js
@@ -1,1 +1,36 @@
-exports.newEmp = (req, res) => {}
+let dbmodel = require("../models/savedeptmodel.js")
+let empCrud = require("../models/empcrudmodel.js")
+
+exports.newEmp = (req, res) => {
+    let promise = dbmodel.getAllDept()
+    promise.then((result) => {
+        res.render("newEmp.ejs",{deptList : result,msg:""})
+    })
+}
+
+exports.saveEmployee = ((req,res) => {
+    let {name,email,contact,sal,deptid} =req.body
+    let filename = req.file.filename
+    
+    let promise = empCrud.saveEmployee(name,email,contact,sal,filename,deptid)
+    promise.then((result) => {
+        let p = dbmodel.getAllDept()
+        p.then((r) => {
+            res.render("newEmp.ejs",{deptList : r,msg:result})
+        })
+    })
+    promise.catch((err) =>{
+        res.send(err)
+    })
+
+})
+
+exports.getAllEmployee = ((req,res) => {
+    let promise = empCrud.getAllEmps()
+        promise.then((result) => {
+            res.render("viewEmp.ejs", { deptList: result })
+        })
+        promise.catch((err) => {
+            res.send(err)
+        })
+})
