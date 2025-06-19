85c2b18fe22f78dddb9597a97d89d82c2253dac1
BariKunal <kunalbari751@gmail.com>
2025-06-19 11:33:43 +0530

    new setup for session

---
+++ Login_Auth_Project/src/app.js
@@ -0,0 +1,14 @@
+let express = require("express")
+let bodyParser = require("body-parser")
+let db = require("../db.js")
+let router = require("./routes/router.js")
+require("dotenv").config()
+let app = express()
+
+app.set("view engine","ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+app.use("/",router)
+
+module.exports = app
