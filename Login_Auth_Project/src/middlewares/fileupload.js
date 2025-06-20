b26d2b375589a77ddfa34c4eef181530e6354a44
BariKunal <kunalbari751@gmail.com>
2025-06-20 10:52:16 +0530

    Fileupload implemented

---
+++ Login_Auth_Project/src/middlewares/fileupload.js
@@ -0,0 +1,13 @@
+let multer = require("multer")
+let strorage = multer.diskStorage({
+    destination : (req,file,cb) => {
+        cb(null,"public/images/")
+    },
+    filename:(req,file,cb) => {
+        cb(null,file.originalname)
+    }
+})
+
+let upload = multer({storage:strorage})
+
+module.exports= upload
