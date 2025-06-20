0de00b1122baebed23b45da1dfde40db586e1cd8
BariKunal <kunalbari751@gmail.com>
2025-06-20 10:51:32 +0530

    import the session

--- Login_Auth_Project/src/app.js
+++ Login_Auth_Project/src/app.js
@@ -1,6 +1,8 @@
 let express = require("express")
 let bodyParser = require("body-parser")
-let db = require("../db.js")
+let conn = require("../db.js")
+let session = require("express-session")
+let path = require("path")
 let router = require("./routes/router.js")
 require("dotenv").config()
 let app = express()
@@ -9,6 +11,12 @@ app.set("view engine","ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
-app.use("/",router)
 
+
+app.use(session({
+    secret: "mySecretKey123",  
+    resave: false,
+    saveUninitialized: true
+}));
+app.use("/",router)
 module.exports = app
