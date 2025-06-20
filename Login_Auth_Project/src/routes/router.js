4498e0e1baf37d402dc856b53355cc844795e69c
BariKunal <kunalbari751@gmail.com>
2025-06-20 10:54:08 +0530

    implemented middleware fileupload

--- Login_Auth_Project/src/routes/router.js
+++ Login_Auth_Project/src/routes/router.js
@@ -1,5 +1,9 @@
 let express = require("express")
 let router = express.Router()
+let upload = require("../middlewares/fileupload.js")
 let ctrl = require("../controllers/loginCtrl.js")
 router.get("/",ctrl.LoginPage)
+router.get("/reg",ctrl.regContr)
+router.post("/regSave",upload.single("photo"),ctrl.saveReg)
+router.post("/validateUser",ctrl.validateLoginUser)
 module.exports = router
