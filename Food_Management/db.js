7e637081aba89288ff68f7e45c4320eaf6b4390b
BariKunal <kunalbari751@gmail.com>
2025-06-27 23:50:10 +0530

    Project file configuration

---
+++ Food_Management/db.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+let mysql = require("mysql2")
+
+let conn = mysql.createConnection({
+    host: process.env.db_host_name,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_database
+})
+
+conn.connect((err) => {
+    if(err){
+        console.log("Database Not Connected..." +err);
+    }
+    else{
+        console.log("Dtabase Connected...");
+    }
+})
+
+module.exports = conn
