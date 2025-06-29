75ef7c2fa5662caf5f87804d73d4874fb18cfcd8
BariKunal <kunalbari751@gmail.com>
2025-06-29 22:10:47 +0530

    model changes for login

--- Login_Auth_Project/src/models/regmodel.js
+++ Login_Auth_Project/src/models/regmodel.js
@@ -1,22 +1,22 @@
 let conn = require("../../db.js")
 
-async function saveReg(...regData){
-    let result = conn.query("insert into register values('0',?,?,?,?,?,?)",[...regData])
+async function saveReg(...regData) {
+    let result = conn.query("insert into register values('0',?,?,?,?,?,?)", [...regData])
     return result
 }
 
-async function validateUser(uname,upass) {
-    let result = conn.query("select * from register where username=? and password=?",[uname,upass])
+async function validateUser(uname, upass) {
+    let result = conn.query("select * from register where username=? and password=?", [uname, upass])
     return result
 }
 
-async function getLoginDetails(loginUserId){
-    let userData = await conn.query("select * from register where rid =?",[loginUserId])
+async function getLoginDetails(loginUserId) {
+    let userData = await conn.query("select * from register where rid =?", [loginUserId])
     return userData
 }
 
-async function Update(name,email,contact,password,loginUserId){
-    let result = await conn.query("update register set name=?,email=?,contact=?,password=? where rid = ?",[name,email,contact,password,loginUserId])
+async function Update(name, email, contact, password, loginUserId) {
+    let result = await conn.query("update register set name=?,email=?,contact=?,password=? where rid = ?", [name, email, contact, password, loginUserId])
     return result
 }
-module.exports={saveReg,validateUser,getLoginDetails,Update}
+module.exports = { saveReg, validateUser, getLoginDetails, Update }
